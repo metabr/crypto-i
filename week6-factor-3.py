49a52d7c06af27aef1f17b2ae815b4d5f72850f2
Aleh Suprunovich <br@ahlamon.org>
2015-02-23 23:44:27 +0300

    Question 3 of PA6

--- week6-factor-3.py
+++ week6-factor-3.py
@@ -9,15 +9,16 @@ N = mpz('72006226374735042527956443552558373833808445147399984182665305798191\
         52463970788523879939683923036467667022162701835329944324119217381272\
         9276147530748597302192751375739387929')
 
-A = gmpy2.isqrt(24 * N)
-x = gmpy2.isqrt((A ** 2) - N)
+A = gmpy2.isqrt(24 * N) + 1
+print "A = %d" % A
 
-p = gmpy2.div(A - x, 6)
-q = gmpy2.div(A + x, 4)
+x = gmpy2.isqrt((A ** 2) - (24 * N))
+print "x = %d" % x
 
-if N == A ** 2 + x ** 2:
-    print "YES"
+p = gmpy2.div(A + x, 4)
+q = gmpy2.div(A - x, 6)
 
-if p * q == N:
-    print p
-    print q
+
+if (p * q) == N:
+    print "p = %d" % p
+    print "q = %d" % q
